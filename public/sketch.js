72a8a4eec30991186aed6bcccd74cc01fec1d7bb
ZH Yang <yohch0101@gmail.com>
2021-11-29 01:21:51 +0800

    test

--- public/sketch.js
+++ public/sketch.js
@@ -179,8 +179,7 @@ function draw(){
     // if(cf2.isChanged) {
     //     socket.emit("test", {roomName: roomName, cf2: cf2.val});
     // }
-    
-      
+
     
     // recieve message
     socket.on("message1", (msg) => {
@@ -189,23 +188,36 @@ function draw(){
     textSize(24);
     fill(0);
     text(msg1, 50, 300);
-}
 
-function getAccel(){
-    DeviceMotionEvent.requestPermission().then(response => {
-        if (response == 'granted') {
-        // Add a listener to get smartphone acceleration 
-            // in the XYZ axes (units in m/s^2)
-            window.addEventListener('devicemotion', (event) => {
-                console.log(event);
-            });
-       // Add a listener to get smartphone orientation 
-           // in the alpha-beta-gamma axes (units in degrees)
-            window.addEventListener('deviceorientation',(event) => {
-                console.log(event);
-            });
-        }
-    });
+    // we can use rotationZ, rotationX and rotationY
+    // they should be used in this order (apparently - see docs)
+    // even though default mode is radians the Z rotation returns degrees unless converted
+
+    // the below code ensures a smooth transition from 0-180 and back
+    let zMotion = round(width / 5 * abs(radians(rotationZ) - PI))
+    // x and y values moved from the centre point
+    let yMotion = round(height / 2 + rotationX * 10)
+    let xMotion = round(width / 2 + rotationY * 10)
+
+    // motion affected circle
+    circle(xMotion, yMotion, zMotion)
+    // reference circle
+    stroke(255)
+    strokeWeight(3)
+    noFill()
+    circle(width / 2, height / 2, width / 1.2)
+
+    // text to provide instructions and
+    // document values at the top of the screen
+    noStroke()
+    textSize(width / 35)
+    textFont("'SFMono-Regular', Consolas, 'Liberation Mono', Menlo, Courier, monospace")
+
+    fill(255, 100, 50)
+    text("click to start on iOS", 10, 80)
+    text("on a mobile: twist, and tilt your device", 10, 120)
+    text("device - x: " + round(rotationX) + ", y: " + round(rotationX) + ", z: " + round(rotationZ), 10, 160)
+    text("circle - x: " + xMotion + ", y: " + yMotion + ", radius: " + zMotion, 10, 200)
 }
 
 function mouseDragged() {
