2c1ddb20f4fb15e02f3b8150e270c3dc8adbaf43
ZH Yang <yohch0101@gmail.com>
2021-11-29 00:03:24 +0800

    test

--- public/sketch.js
+++ public/sketch.js
@@ -12,11 +12,57 @@ let msg1;
 let value = 0;
 let permissionGranted = false;
 let motion = false, ios = false;
-let flock;
+
+// this class describes the properties of a single particle.
+class Particle {
+    // setting the co-ordinates, radius and the
+    // speed of a particle in both the co-ordinates axes.
+    constructor(){
+        this.x = random(0,width);
+        this.y = random(0,height);
+        this.r = random(1,8);
+        this.xSpeed = random(-2,2);
+        this.ySpeed = random(-1,1.5);
+    }
+    
+    // creation of a particle.
+    createParticle() {
+        noStroke();
+        fill('rgba(200,169,169,0.5)');
+        circle(this.x,this.y,this.r);
+    }
+    
+    // setting the particle in motion.
+    moveParticle() {
+        if(this.x < 0 || this.x > width)
+            this.xSpeed*=-1;
+        if(this.y < 0 || this.y > height)
+            this.ySpeed*=-1;
+        this.x+=this.xSpeed;
+        this.y+=this.ySpeed;
+    }
+    
+    // this function creates the connections(lines)
+    // between particles which are less than a certain distance apart
+    joinParticles(particles) {
+        particles.forEach(element =>{
+            let dis = dist(this.x,this.y,element.x,element.y);
+            if(dis<85) {
+              stroke('rgba(255,255,255,0.04)');
+              line(this.x,this.y,element.x,element.y);
+            }
+        });
+    }
+}
+
+// an array to add multiple particles
+let particles = [];
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    
+    for(let i = 0;i<width/10;i++){
+        particles.push(new Particle());
+    }
     
     // gui = createGui(); // call the gui objects
     // gui.loadStyle("TerminalMagenta");
@@ -55,12 +101,7 @@ function setup(){
     //     ios = false;
     //   }
 
-    flock = new Flock();
-    // Add an initial set of boids into the system
-    for (let i = 0; i < 100; i++) {
-        let b = new Boid(width / 2,height / 2);
-        flock.addBoid(b);
-    }    
+    
 
     // when join the room send the room name to server
     socket.emit("join_room", roomName);
@@ -92,8 +133,12 @@ function newDrawing(data) {
 
 
 function draw(){
-    background(51);
-    flock.run();
+    background("#0f0f0f");
+    for(let i = 0;i<particles.length;i++) {
+        particles[i].createParticle();
+        particles[i].moveParticle();
+        particles[i].joinParticles(particles.slice(i));
+    }
     
     // drawGui(); // need to be in draw(), and after the background()
     // backToHome() //if home is pressed then back to home page
@@ -140,7 +185,7 @@ function getAccel(){
 }
 
 function mouseDragged() {
-    flock.addBoid(new Boid(mouseX, mouseY));
+    
     console.log("Sending: " + mouseX + "," + mouseY);
     
     let data = {
@@ -154,193 +199,6 @@ function mouseDragged() {
     // fill(255);
     // ellipse(mouseX, mouseY, 36, 36);
 }
-
-function Flock() {
-    // An array for all the boids
-    this.boids = []; // Initialize the array
-  }
-
-Flock.prototype.run = function() {
-    for (let i = 0; i < this.boids.length; i++) {
-        this.boids[i].run(this.boids);  // Passing the entire list of boids to each boid individually
-    }
-}
-  
-Flock.prototype.addBoid = function(b) {
-    this.boids.push(b);
-}
-
-// The Nature of Code
-// Daniel Shiffman
-// http://natureofcode.com
-
-// Boid class
-// Methods for Separation, Cohesion, Alignment added
-
-function Boid(x, y) {
-    this.acceleration = createVector(0, 0);
-    this.velocity = createVector(random(-1, 1), random(-1, 1));
-    this.position = createVector(x, y);
-    this.r = 3.0;
-    this.maxspeed = 3;    // Maximum speed
-    this.maxforce = 0.05; // Maximum steering force
-  }
-  
-  Boid.prototype.run = function(boids) {
-    this.flock(boids);
-    this.update();
-    this.borders();
-    this.render();
-  }
-  
-  Boid.prototype.applyForce = function(force) {
-    // We could add mass here if we want A = F / M
-    this.acceleration.add(force);
-  }
-  
-  // We accumulate a new acceleration each time based on three rules
-  Boid.prototype.flock = function(boids) {
-    let sep = this.separate(boids);   // Separation
-    let ali = this.align(boids);      // Alignment
-    let coh = this.cohesion(boids);   // Cohesion
-    // Arbitrarily weight these forces
-    sep.mult(1.5);
-    ali.mult(1.0);
-    coh.mult(1.0);
-    // Add the force vectors to acceleration
-    this.applyForce(sep);
-    this.applyForce(ali);
-    this.applyForce(coh);
-  }
-  
-  // Method to update location
-  Boid.prototype.update = function() {
-    // Update velocity
-    this.velocity.add(this.acceleration);
-    // Limit speed
-    this.velocity.limit(this.maxspeed);
-    this.position.add(this.velocity);
-    // Reset accelertion to 0 each cycle
-    this.acceleration.mult(0);
-  }
-  
-  // A method that calculates and applies a steering force towards a target
-  // STEER = DESIRED MINUS VELOCITY
-  Boid.prototype.seek = function(target) {
-    let desired = p5.Vector.sub(target,this.position);  // A vector pointing from the location to the target
-    // Normalize desired and scale to maximum speed
-    desired.normalize();
-    desired.mult(this.maxspeed);
-    // Steering = Desired minus Velocity
-    let steer = p5.Vector.sub(desired,this.velocity);
-    steer.limit(this.maxforce);  // Limit to maximum steering force
-    return steer;
-  }
-  
-  Boid.prototype.render = function() {
-    // Draw a triangle rotated in the direction of velocity
-    let theta = this.velocity.heading() + radians(90);
-    fill(127);
-    stroke(200);
-    push();
-    translate(this.position.x, this.position.y);
-    rotate(theta);
-    beginShape();
-    vertex(0, -this.r * 2);
-    vertex(-this.r, this.r * 2);
-    vertex(this.r, this.r * 2);
-    endShape(CLOSE);
-    pop();
-  }
-  
-  // Wraparound
-  Boid.prototype.borders = function() {
-    if (this.position.x < -this.r)  this.position.x = width + this.r;
-    if (this.position.y < -this.r)  this.position.y = height + this.r;
-    if (this.position.x > width + this.r) this.position.x = -this.r;
-    if (this.position.y > height + this.r) this.position.y = -this.r;
-  }
-  
-  // Separation
-  // Method checks for nearby boids and steers away
-  Boid.prototype.separate = function(boids) {
-    let desiredseparation = 25.0;
-    let steer = createVector(0, 0);
-    let count = 0;
-    // For every boid in the system, check if it's too close
-    for (let i = 0; i < boids.length; i++) {
-      let d = p5.Vector.dist(this.position,boids[i].position);
-      // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
-      if ((d > 0) && (d < desiredseparation)) {
-        // Calculate vector pointing away from neighbor
-        let diff = p5.Vector.sub(this.position, boids[i].position);
-        diff.normalize();
-        diff.div(d);        // Weight by distance
-        steer.add(diff);
-        count++;            // Keep track of how many
-      }
-    }
-    // Average -- divide by how many
-    if (count > 0) {
-      steer.div(count);
-    }
-  
-    // As long as the vector is greater than 0
-    if (steer.mag() > 0) {
-      // Implement Reynolds: Steering = Desired - Velocity
-      steer.normalize();
-      steer.mult(this.maxspeed);
-      steer.sub(this.velocity);
-      steer.limit(this.maxforce);
-    }
-    return steer;
-  }
-  
-  // Alignment
-  // For every nearby boid in the system, calculate the average velocity
-  Boid.prototype.align = function(boids) {
-    let neighbordist = 50;
-    let sum = createVector(0,0);
-    let count = 0;
-    for (let i = 0; i < boids.length; i++) {
-      let d = p5.Vector.dist(this.position,boids[i].position);
-      if ((d > 0) && (d < neighbordist)) {
-        sum.add(boids[i].velocity);
-        count++;
-      }
-    }
-    if (count > 0) {
-      sum.div(count);
-      sum.normalize();
-      sum.mult(this.maxspeed);
-      let steer = p5.Vector.sub(sum, this.velocity);
-      steer.limit(this.maxforce);
-      return steer;
-    } else {
-      return createVector(0, 0);
-    }
-  }
-  
-  // Cohesion
-  // For the average location (i.e. center) of all nearby boids, calculate steering vector towards that location
-  Boid.prototype.cohesion = function(boids) {
-    let neighbordist = 50;
-    let sum = createVector(0, 0);   // Start with empty vector to accumulate all locations
-    let count = 0;
-    for (let i = 0; i < boids.length; i++) {
-      let d = p5.Vector.dist(this.position,boids[i].position);
-      if ((d > 0) && (d < neighbordist)) {
-        sum.add(boids[i].position); // Add location
-        count++;
-      }
-    }
-    if (count > 0) {
-      sum.div(count);
-      return this.seek(sum);  // Steer towards the location
-    } else {
-      return createVector(0, 0);
-    }
-  }
 
 
 
